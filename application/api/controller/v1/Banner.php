e1c042db4628706e58ab0c987f647397318a20df
yumeng <530881987@qq.com>
2020-03-26 23:07:47 +0800

    新建api模块 2020-03-26

---
+++ application/api/controller/v1/Banner.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @url
+ * @http
+ * User: yumeng
+ * CreateTime: 2020/3/26 20:48
+ * Description:
+ */
+
+namespace app\api\controller\v1;
+
+
+class Banner
+{
+    /**
+     * @return string
+     *
+     * Description:
+     * Author: yumeng
+     * CreateTime: 2020/3/26 21:13
+     */
+    
+    public function getBanner(){
+        
+        return 'dadad';
+    }
+}

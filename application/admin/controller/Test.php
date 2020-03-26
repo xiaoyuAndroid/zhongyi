c5e424f952dce750cba4d00cd9458a34b009845d
yumeng <root@localhost.localdomain>
2020-03-26 10:21:45 +0800

    2020-03-26 测试控制器

---
+++ application/admin/controller/Test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yumeng
+ * Date: 2020/3/24
+ * Time: 22:42
+ */
+
+namespace app\admin\controller;
+
+
+class Test
+{
+    public function hello()
+    {
+        return "nihao,qiyue";
+    }
+    
+    
+}

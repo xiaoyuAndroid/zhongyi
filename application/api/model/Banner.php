be4dec476cce63d9f83fcc52f25d439ae21c219e
yumeng <530881987@qq.com>
2020-04-01 23:34:54 +0800

    数据库学习 2020-04-01

--- application/api/model/Banner.php
+++ application/api/model/Banner.php
@@ -10,6 +10,7 @@
 namespace app\api\model;
 
 
+use think\Db;
 use think\Exception;
 
 class Banner
@@ -17,6 +18,7 @@ class Banner
     public static function getBannerByID($id)
     {
         //return 'this is banner info';
+        Db::query();
         return null;
     }
 }

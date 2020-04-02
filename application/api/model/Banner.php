b1bb60eff4f7f047f2d73fa2f69d864b0b72317a
yumeng <530881987@qq.com>
2020-04-02 23:41:18 +0800

    ...

--- application/api/model/Banner.php
+++ application/api/model/Banner.php
@@ -12,8 +12,9 @@ namespace app\api\model;
 
 use think\Db;
 use think\Exception;
+use think\Model;
 
-class Banner
+class Banner extends Model
 {
     public static function getBannerByID($id)
     {

ecf1e25ffb5e0b16f13ffb18739a49f8fb95f502
yumeng <530881987@qq.com>
2020-04-04 20:16:13 +0800

    新建BaseModel类

--- application/api/model/Image.php
+++ application/api/model/Image.php
@@ -4,17 +4,12 @@ namespace app\api\model;
 
 use think\Model;
 
-class Image extends Model
+class Image extends BaseModel
 {
     //$value就是Image这个表的Url字段，$data是一条记录的完整字段
     public function getUrlAttr($value, $data)
     {
-        $finalUrl = $value;
-        if ( $data['from'] == 1 ) {
-            $finalUrl = config('setting.img_url') . $value;
-        }
-        
-        return $finalUrl;
+        return $this->prefixImgUrl($value,$data);
     }
     //
 }

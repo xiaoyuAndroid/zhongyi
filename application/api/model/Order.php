05b8e556157df54e0a4e0cbe3b9858a630fa3f8f
yumeng <530881987@qq.com>
2020-04-14 23:41:33 +0800

    下单接口 2020-04-14

---
+++ application/api/model/Order.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @url
+ * @http
+ * Author: yumeng
+ * CreateTime: 2020/4/14 21:18
+ * Description:
+ */
+
+namespace app\api\model;
+
+
+class Order extends BaseModel
+{
+    
+}
---
+++ application/api/service/Order.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @url
+ * @http
+ * Author: yumeng
+ * CreateTime: 2020/4/14 19:28
+ * Description:
+ */
+
+namespace app\api\service;
+
+
+class Order
+{
+    
+}
